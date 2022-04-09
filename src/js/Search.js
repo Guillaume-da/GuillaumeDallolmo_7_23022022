e5bf00a74d0b4bf60d11003c892b3a9e4c3f874f
Guillaume-da <singy@hotmail.fr>
2022-04-09 11:14:08 +0200

    Dropdowns lists sorting fixed

--- src/js/Search.js
+++ src/js/Search.js
@@ -463,7 +463,9 @@ export class Search {
 		if(!list.includes(item)){
 			list.push(item);
 		}
-		list.sort();
+		list.sort((a, b) =>{
+			return a.localeCompare(b);
+		});
 	}
 
 	getListDatas(recipes) {
